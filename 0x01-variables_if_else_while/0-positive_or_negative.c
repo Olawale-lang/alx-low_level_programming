a07d13ae8299491f4773279c54905a6444d91722
Afeez Olawale <bamideleolawale13@gmil.com>
2023-02-19 16:54:21 -0800

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	int;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -21,7 +21,7 @@ int main(void)
 	else if (n == 0)
 		printf("%d is zero", n);
 	else
-		printf("%d is negative", n)
+		printf("%d is negative", n);
 	printf("\n");
 	return (0);
 }
